0f20fdcf54ce666df550f057ef2e72484013d8b7
Nicolas Jacquemin <jacquemin.nicolas59@gmail.com>
2024-06-06 14:22:05 +0200

    feat nest - use compression

--- MonkeyBasicMuxAudio/monkey_diffusion_api/src/main.ts
+++ MonkeyBasicMuxAudio/monkey_diffusion_api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
+  app.use(compression());
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
   await app.listen(3000);
 }
 bootstrap();
