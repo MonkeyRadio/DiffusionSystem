312f1c995197a15123249551939c9f34567df453
Nicolas Jacquemin <jacquemin.nicolas59@gmail.com>
2024-06-06 14:43:42 +0200

    feat - add nginx to proxy all services

--- MonkeyBasicMuxAudio/monkey_diffusion_api/src/main.ts
+++ MonkeyBasicMuxAudio/monkey_diffusion_api/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
-  app.use(compression());
   app.getHttpAdapter().getInstance().disable('x-powered-by');
   await app.listen(3000);
 }
