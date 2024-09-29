b17831db712681f783d44ca932a27051b7c95de0
Nicolas Jacquemin <jacquemin.nicolas59@gmail.com>
2024-09-29 12:40:06 +0200

    fix(diff-api) - wrong CDN_URL env name

--- monkey_diffusion_api/src/metadata/metadata.service.ts
+++ monkey_diffusion_api/src/metadata/metadata.service.ts
@@ -42,7 +42,7 @@ export class MetadataService {
       year: metadata.year,
       trackNumber: metadata.trackNumber,
       duration: metadata.duration,
-      imageUrl: `${this.configService.get('cdnUrl')}/radio-${radioId}/${metadata.internalId.replace(strRegex, '')}`,
+      imageUrl: `${this.configService.get<string>('CDN_URL')}/radio-${radioId}/${metadata.internalId.replace(strRegex, '')}`,
       tsPosted: metadata.tsPosted,
       comment: {
         internalId: metadata.internalId.replace(strRegex, ''),
